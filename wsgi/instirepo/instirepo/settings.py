a7498e52bf674e46c742ac50053dba5274d0d94f
Ashish Goel <ashishg656@gmail.com>
2015-10-04 00:04:30 +0530

    change name

--- wsgi/instirepo/instirepo/settings.py
+++ wsgi/instirepo/instirepo/settings.py
@@ -1,5 +1,5 @@
 """
-Django settings for myproject project.
+Django settings for instirepo project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/topics/settings/
@@ -60,8 +60,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-# GETTING-STARTED: change 'myproject' to your project name:
-ROOT_URLCONF = 'myproject.urls'
+# GETTING-STARTED: change 'instirepo' to your project name:
+ROOT_URLCONF = 'instirepo.urls'
 
 TEMPLATES = [
     {
@@ -79,7 +79,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myproject.wsgi.application'
+WSGI_APPLICATION = 'instirepo.wsgi.application'
 
 
 # Database
