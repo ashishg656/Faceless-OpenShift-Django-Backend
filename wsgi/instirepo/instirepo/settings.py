d1eaaca06b6e9371163382052cb5c3d6fd94012f
Ashish Goel <ashishg656@gmail.com>
2015-10-16 15:38:57 +0530

    datetimee

--- wsgi/instirepo/instirepo/settings.py
+++ wsgi/instirepo/instirepo/settings.py
@@ -75,11 +75,11 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Calcutta'
 
-# USE_I18N = True
+USE_I18N = True
 
-# USE_L10N = True
+USE_L10N = True
 
-# USE_TZ = True
+USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
